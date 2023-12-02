fc80e88997a5bf70137f5dee637e2cd15f3129a8
Natan16 <natanvianat16@gmail.com>
2023-12-01 21:51:53 -0300

    Handles icon generation and geolocation

--- app/src/main/java/com/application/mercado/MainActivity.kt
+++ app/src/main/java/com/application/mercado/MainActivity.kt
@@ -1,25 +1,32 @@
 package com.application.mercado
 
 import android.os.Bundle
+import android.webkit.GeolocationPermissions
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.application.mercado.ui.theme.MercadoSimplificadoTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val myWebView = WebView(getApplicationContext())
-        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.setJavaScriptEnabled(true);
+        myWebView.settings.setGeolocationEnabled(true);
+        myWebView.setWebChromeClient(object : WebChromeClient() {
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String,
+                callback: GeolocationPermissions.Callback
+            ) {
+                callback.invoke(origin, true, false)
+            }
+        })
         setContentView(myWebView)
-
         myWebView.loadUrl("https://mercadosimplificado.com");
     }
 }
